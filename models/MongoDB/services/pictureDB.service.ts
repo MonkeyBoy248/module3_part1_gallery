0f4d05785c3ae0b5fb705e0feafd9788eafe8b38
Kirill Temnyi <kirik201100@mail.ru>
2022-04-26 15:12:04 +0300

    feat: create service that works with picture data
    pictureDB service contains six methods that responsible for adding default pictures to the DB, adding a new picture to the DB, and calculating general documents amount, retrieving specific user pictures from the DB, calculating amount of pictures of a specific user
    
    MODULE-3-PART-1

---
+++ models/MongoDB/services/pictureDB.service.ts
@@ -0,0 +1,68 @@
+import { Picture } from "@interfaces/picture.interface";
+import { PictureModel } from "@models/MongoDB/picture.model";
+import { FileService } from "@services/file.service";
+import { ObjectId } from "mongodb";
+import { HttpBadRequestError, HttpInternalServerError } from "@floteam/errors";
+
+export class PicturesDBService {
+  private fileService = new FileService();
+
+  addPicturesToTheDB = async () => {
+    try {
+      const pictureNames = await this.fileService.getFileNames() || [];
+      const pictureMetadata = await this.fileService.getFilesMetadata();
+      const newPicturesList: Picture[] = [];
+
+
+      for (let i = 0; i < pictureNames.length; i++) {
+        if (await PictureModel.exists({path: pictureNames[i]}) === null) {
+          newPicturesList.push({
+            path: pictureNames[i],
+            metadata: pictureMetadata[i],
+            owner: null,
+          })
+        }
+      }
+
+      await Promise.all(newPicturesList.map(async (item) => {
+        await PictureModel.create(item);
+        console.log(`item ${item} added`);
+      }));
+    } catch (err) {
+     throw new HttpBadRequestError('Picture already exists')
+    }
+  }
+
+  addUserPicturesToDB = async (pictureObject: Picture) => {
+    try {
+      await PictureModel.create(pictureObject);
+    } catch (err) {
+      throw new HttpInternalServerError('Failed to upload picture', err.message)
+    }
+  }
+
+  getTotalImagesAmount = async () => {
+    return await PictureModel.estimatedDocumentCount();
+  }
+
+  getPicturesFromDB = async (ownerId: ObjectId, page : number, limit: number, filter: string) => {
+    let filterQuery = filter === 'false' ? {$or: [{owner: null}, {owner: ownerId}]} : {owner: ownerId};
+
+    try {
+      return await PictureModel.find(filterQuery, null, {skip: limit * page - limit, limit: limit});
+    } catch (err) {
+      throw new HttpInternalServerError('Failed to get pictures ', err.message);
+    }
+
+  }
+
+  getPicturesAmount = async (id: ObjectId, filter: string) => {
+    let filterQuery = filter === 'false' ? {$or: [{owner: null}, {owner: id}]} : {owner: id};
+
+    return await PictureModel.countDocuments(filterQuery);
+  }
+
+  isUserPicturesEmpty = async (id: ObjectId) => {
+    return await PictureModel.countDocuments({owner: id}) === 0;
+  }
+}
