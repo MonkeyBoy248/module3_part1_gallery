41c4d1ee363251546487a7757dd47e388e1347b5
Kirill Temnyi <kirik201100@mail.ru>
2022-05-05 11:43:33 +0300

    chore: edit import path for DbConnectionError file
    MODULE-3-PART-1

--- models/MongoDB/services/mongoConnection.service.ts
+++ models/MongoDB/services/mongoConnection.service.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import {DbConnectionError} from "../errors/dbConnection.error";
+import {DbConnectionError} from "../../../errors/dbConnection.error";
 
 class MongoDBConnectionService {
   private readonly mongoUrl = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@${process.env.MONGO_CLUSTER}/myFirstDatabase?retryWrites=true&w=majority` || '';
 
-  connectDB =  async () => {
+  connectDB = async () => {
     try {
       const dbConnection = await mongoose.connect(this.mongoUrl);
 
